98583d66cbd0fe95b871eff9f758a5954ee70a76
Rachel <rachelshenhom@gmail.com>
2021-04-20 12:30:53 -0400

    lists

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// if(typeof jQuery == 'undefined') {
-//     console.log('oops! I still have to link my jQuery properly');  
-// }
-// else {console.log('I did it! I linked jQuery and this is file!')};
-const $container = $('<container>');
-$('#container')
-console.log('#container');
-//create an h1 element and set it to var called $h1
-const $h1 = $('<h1>')
-console.log('$h1');
-//add Hogwars text inside h1 element 
-$('h1').text('Hogwarts')
+
+// const $container = $('<container>');
+// $('#container')
+// console.log('#container');
+// //create an h1 element and set it to var called $h1
+// const $h1 = $('<h1>')
+// console.log('$h1');
+// //add Hogwars text inside h1 element 
+// $('h1').text('Hogwarts')
+// const $div = $('div');
+//create h2 element with my name
+// const $h2 = $('h2');
