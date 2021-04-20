b313929b323e39ed2ceac164dc85e77630b4f70b
Rachel <rachelshenhom@gmail.com>
2021-04-20 12:11:11 -0400

    text

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 //     console.log('oops! I still have to link my jQuery properly');  
 // }
 // else {console.log('I did it! I linked jQuery and this is file!')};
-
+const $container = $('<container>');
+$('#container')
+console.log('#container');
+//create an h1 element and set it to var called $h1
+const $h1 = $('<h1>')
+console.log('$h1');
+//add Hogwars text inside h1 element 
+$('h1').text('Hogwarts')
